c4fd2c3fde1ee695238fd94b8b1aab0f3a2705eb
Ziemin <brentprey@gmail.com>
2014-02-10 10:21:08 +0100

    Added graph generator

---
+++ src/examples/graph_stream_triangle_count/utils/graph_generate.cpp
@@ -0,0 +1,64 @@
+#include <random>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <memory>
+
+using namespace std;
+
+vector<vector<int>> graph;
+
+inline void add_edge(int u, int v) {
+    graph[u][v] = 1;
+    graph[v][u] = 1;
+}
+
+inline bool is_edge(int u, int v) {
+    return graph[u][v];
+}
+
+int main(int argc, char* argv[]) {
+    int vc = stoi(argv[1]);
+
+    graph.resize(vc);
+    for(auto& g: graph) {
+        g.resize(vc);
+        fill(begin(g), end(g), 0);
+    }
+
+    int fn = stoi(argv[2]);
+    string bfname = "edges_";
+    unique_ptr<ofstream> *files = new unique_ptr<ofstream>[fn];
+
+    for(int i = 0; i < fn; i++) files[i].reset(new ofstream(bfname + to_string(i+1)));
+
+    mt19937 rng;
+    uniform_int_distribution<int> dist(0, vc-1);
+    for(int i = 0; i < vc*vc; i++) {
+        if(dist(rng)%2) {
+            int u = dist(rng);
+            int v = dist(rng);
+            if(!is_edge(u,v)) {
+                add_edge(u,v);
+                *files[i%fn] << u << " " << v;
+            }
+        }
+    }
+
+    int counter = 0;
+    for(int i = 0; i < vc; i++) {
+        for(int j = 0; j < i; j++) {
+            for(int k = 0; k < j; k++) {
+                if(is_edge(i, j) && is_edge(j, k) && is_edge(i,k)) counter++;
+            }
+        }
+    }
+    std::cout << "Triangles: " << counter << std::endl;
+
+    delete [] files;
+
+    return 0;
+}
